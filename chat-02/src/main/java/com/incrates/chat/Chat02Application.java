3e4cc4709f0a193aaf7e59ef7b9b5e84a49c5c72
jin_crates <94jingyu@gmail.com>
2021-07-21 21:29:29 +0900

    chat-03 commit

---
+++ chat-02/src/main/java/com/incrates/chat/Chat02Application.java
@@ -0,0 +1,13 @@
+package com.incrates.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chat02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chat02Application.class, args);
+	}
+
+}
---
+++ chat-02/src/test/java/com/incrates/chat/Chat02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incrates.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chat02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
